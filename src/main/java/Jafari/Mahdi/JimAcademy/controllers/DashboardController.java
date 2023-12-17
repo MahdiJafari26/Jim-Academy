a08dc4f7c57d2cbe07e8287223d1db66cbe7bf2a
MahdiJafari26 <mahdijafari1379@gmail.com>
2023-12-17 20:45:13 +0330

    Designing dashboard

--- src/main/java/Jafari/Mahdi/JimAcademy/controllers/DashboardController.java
+++ src/main/java/Jafari/Mahdi/JimAcademy/controllers/DashboardController.java
@@ -1,6 +1,5 @@
 package Jafari.Mahdi.JimAcademy.controllers;
 
-import Jafari.Mahdi.JimAcademy.carriers.WebToast;
 import Jafari.Mahdi.JimAcademy.entities.User;
 import Jafari.Mahdi.JimAcademy.repositories.UserRepository;
 import jakarta.servlet.http.HttpSession;
@@ -11,11 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
-
-import java.util.List;
-import java.util.Optional;
 
 
 @Controller
@@ -38,13 +32,14 @@ public class DashboardController {
 
     public ModelAndView returnUserValidationModel(String title, String viewName, ModelMap map) {
         User sessionUser = ((User) session.getAttribute("currentUser"));
-        if (sessionUser !=  null)
-        for (User tmpUser : userRepository.findAll()) {
-            if (sessionUser.getId() == tmpUser.getId() && sessionUser.getUsername().equals(tmpUser.getUsername()) && sessionUser.getPassword().equals(tmpUser.getPassword())) {
-                map.put("title", title);
-                return new ModelAndView(viewName, map);
+        if (sessionUser != null)
+            for (User tmpUser : userRepository.findAll()) {
+                if (sessionUser.getId() == tmpUser.getId() && sessionUser.getUsername().equals(tmpUser.getUsername()) && sessionUser.getPassword().equals(tmpUser.getPassword())) {
+                    map.put("title", title);
+                    map.put("user" , tmpUser);
+                    return new ModelAndView(viewName, map);
+                }
             }
-        }
         return new ModelAndView("redirect:/login", map);
     }
 }
